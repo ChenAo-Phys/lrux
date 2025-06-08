35f7abd87d29277345659cc5acbec218d894b5ba
ChenAo-Phys <chenao@ROGAC.localdomain>
2025-06-08 11:56:37 -0700

    pf_lru format

--- lrux/pf_lru.py
+++ lrux/pf_lru.py
@@ -1,8 +1,23 @@
 from typing import Optional, Tuple, Union
+from jax import Array
 import jax
 import jax.numpy as jnp
 import jax.tree_util as jtu
+from .det_lru import _check_mat, _standardize_uv
 from .pfaffian import pf
+
+
+def pf_lru(
+    Ainv: Array,
+    u: Union[Array, Tuple[Array, Array]],
+    return_update: bool = False,
+) -> Union[Array, Tuple[Array, Array]]:
+    """
+    Low-rank update of determinant
+    """
+    _check_mat(Ainv)
+    u = _standardize_uv(u, Ainv.shape[0], Ainv.dtype)
+    pass
 
 
 @jtu.register_pytree_node_class
@@ -11,40 +26,41 @@ class PfCarrier:
     The pytree carrying intermediate information for low-rank updates.
     """
 
-    def __init__(
-        self,
-        inv: jax.Array,
-        a: Optional[jax.Array] = None,
-        b: Optional[jax.Array] = None,
-        current_delay: Optional[jax.Array] = None,
-    ):
-        self.inv = inv
+    def __init__(self, Ainv: jax.Array, a: jax.Array, Rinv: jax.Array):
+        self.inv = Ainv
         self.a = a
-        self.b = b
-        self.current_delay = current_delay
+        self.Rinv = Rinv
 
     def tree_flatten(self) -> Tuple:
-        children = (
-            self.inv,
-            self.a,
-            self.b,
-            self.current_delay,
-        )
+        children = (self.inv, self.a, self.Rinv)
         aux_data = None
         return (children, aux_data)
 
     @classmethod
     def tree_unflatten(cls, aux_data, children):
         return cls(*children)
-    
-
-def init_pf_carrier():pass
 
 
-def pf_ratio(
-    carrier: Union[jax.Array, PfCarrier],
-    u: jax.Array,
-    e: jax.Array,
+def init_pf_carrier(A: Array, max_delay: int, max_rank: int = 1) -> PfCarrier:
+    if max_delay <= 0:
+        raise ValueError(
+            "`max_delay` should be a positive integer. "
+            "Otherwise, please use `det_lru` for non-delayed updates."
+        )
+    _check_mat(A)
+    Ainv = jnp.linalg.inv(A)
+    a = jnp.zeros((max_delay, A.shape[0], max_rank), A.dtype)
+    Rinv = jnp.zeros((max_delay, max_rank, max_rank), A.dtype)
+    return PfCarrier(Ainv, a, Rinv)
+
+
+def pf_lru_delayed(
+    carrier: PfCarrier,
+    u: Union[int, Array, Tuple[Array, Array]],
     return_update: bool = False,
-) -> Union[jax.Array, Tuple[jax.Array, jax.Array]]:
+    current_delay: Optional[int] = None,
+) -> Union[Array, Tuple[Array, PfCarrier]]:
+    """
+    Low-rank update of pfaffian with delayed updates
+    """
     pass
